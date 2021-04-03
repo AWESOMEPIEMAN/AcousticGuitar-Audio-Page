419ab3fc2b4173fea92e40508862408f0f9540ab
Usman Hamid <55622390+AWESOMEPIEMAN@users.noreply.github.com>
2021-04-03 16:03:16 +0500

    Add files via upload

---
+++ music.js
@@ -0,0 +1,42 @@
+        var E = new Audio();
+        E.src = "Music file/Plucking/E.mp3"
+        var Elow = new Audio();
+        Elow.src = "Music file/Plucking/E low.mp3"
+        var A = new Audio();
+        A.src = "Music file/Plucking/A.mp3"
+        var D = new Audio();
+        D.src = "Music file/Plucking/D.mp3"
+        var G = new Audio();
+        G.src = "Music file/Plucking/G.mp3"
+        var B = new Audio();
+        B.src = "Music file/Plucking/b.mp3"
+
+        var cchord = new Audio();
+        cchord.src = "Music file/Strum Pick/C pick.mp3"
+
+        var dchord = new Audio();
+        dchord.src = "Music file/Strum Pick/D pick.mp3"
+
+        var emchord = new Audio();
+        emchord.src = "Music file/Strum Pick/E minor pick.mp3"
+
+        var gchord = new Audio();
+        gchord.src = "Music file/Strum Pick/G major pick.mp3"
+
+        var cfchord = new Audio();
+        cfchord.src = "Music file/Strum fing/C fing.mp3"
+
+        var dfchord = new Audio();
+        dfchord.src = "Music file/Strum fing/D fing.mp3"
+
+        var emfchord = new Audio();
+        emfchord.src = "Music file/Strum fing/e min fing.mp3"
+
+        var gcfhord = new Audio();
+        gcfhord.src = "Music file/Strum fing/G maj fing.mp3"
+
+        var t1 = new Audio();
+        t1.src = "Music file/Tunes/Tune 1.mp3"
+
+        var t2 = new Audio();
+        t2.src = "Music file/Tunes/Tune 2.mp3"
